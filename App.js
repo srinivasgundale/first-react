6645af1a17030911a170940a77b7f95389127ac5
Srinivas <srinivas.gundali@accenture.com>
2024-03-21 23:53:48 +0800

    Learning - Changing react element to jsx

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
 const heading = React.createElement("h1",{id:'heading', class:"heading"},"Hello World");
+const jsxHeading = <h1 id='heading'>React JSX</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(jsxHeading);
