70521b58784cd47d427384ff05f81a4b9f40bd99
Srinivas <srinivas.gundali@accenture.com>
2024-03-22 00:59:52 +0800

    Learning - React Functional Component - Create - Render

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
+// React Element (RE)
 const heading = React.createElement("h1",{id:'heading', class:"heading"},"Hello World");
-const jsxHeading = <h1 id='heading'>React JSX</h1>
+// React JSX -> -> parcel -> Bable -> RCE  -> RE (JS object) -> Render to browser with render method
+const jsxHeading = (<h1 id='heading'>
+    React JSX
+</h1>)
+const HeadingFuncComponent = () => {
+    return <h1>This is headingFuncComponent</h1>
+};
+const HeadingFuncComponent2 = () => <h1>This is headingFuncComponent</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxHeading);
+root.render(<HeadingFuncComponent />);
