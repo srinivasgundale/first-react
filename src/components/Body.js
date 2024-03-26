a3ee064a2f7fee97b7793f3481cab5c6dfff6e27
Srinivas <srinivas.gundali@accenture.com>
2024-03-26 14:00:06 +0800

    Learning & Practice - React hooks with useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,21 @@
 import RestaurantCard from "./RestaurantCard"
 import resList from "./../utils/mockdata"
+import { useState } from 'react'
 const Body = () => {
+    const [listOfRest, setListOfRest] = useState(resList);
     return (
       <div className="body">
+        <div className="filter">
+            <button className="filter-btn" onClick={() => {
+                const filteredList = listOfRest.filter(
+                    (res) => res.data.avgRating <= 4
+                );
+                setListOfRest(filteredList);
+            }}>Filter</button>
+        </div>
         <div className="search">Search</div>
         <div className="res-container">
-          {resList.map((restaurant) => (
+          {listOfRest.map((restaurant) => (
             <RestaurantCard key={restaurant.data.id} resData={restaurant} />
           ))}
         </div>
