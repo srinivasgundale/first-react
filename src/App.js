f4bb62b69e2063cf3422858170c910f42dde8412
Srinivas <srinivas.gundali@accenture.com>
2024-04-02 11:43:48 +0800

    Lazy loading or dynamic bundle loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "bootstrap/dist/css/bootstrap.css";
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 
 import Header from "./components/Head";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import AboutUs from "./components/AboutUs";
+//import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
 import Error from "./components/Error";
 import ProductDetail from "./components/ProductDetail";
+const AboutUs = lazy(() => import("./components/AboutUs"));
 const AppLayout = () => {
   return (
     <div className="app">
@@ -34,7 +35,11 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about-us",
-        element: <AboutUs />,
+        element: (
+          <Suspense fallback={<h1>Loading</h1>}>
+            <AboutUs />
+          </Suspense>
+        ),
       },
       {
         path: "/product/:id",
