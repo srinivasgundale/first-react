65de453c368aa0e4b739c204a0506454b144ad10
Srinivas Gundali <srinivasgundale@gmail.com>
2024-08-22 12:32:23 +0800

    card layout update

--- src/components/includes/RestaurantCard.jsx
+++ src/components/includes/RestaurantCard.jsx
@@ -147,9 +147,10 @@ const RestaurantCard = ({ resData, isSponsored }) => {
     //       </button>
     //     </div>
     //   </div>
-      
+
     // </>
-    <Link to={"/product/" + id}>
+    <>
+      {/* <Link to={"/product/" + id}>
     <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
         <div className="flex items-end justify-end h-56 w-full bg-cover" style={{backgroundImage: `url(${thumbnail})`}}> 
         <button
@@ -179,16 +180,104 @@ const RestaurantCard = ({ resData, isSponsored }) => {
               </>
             )}
           </button>
-            {/* <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-                <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
-            </button> */}
+            
         </div>
         <div className="px-5 py-3">
             <h3 className="text-gray-700 uppercase">{title?.substring(0, 20)}...</h3>
             <span className="text-gray-500 mt-2">${price}</span>
         </div>
     </div>
-    </Link>
+    </Link> */}
+      {/* <Link to={"/product/" + id}>
+    <div className="card card-compact bg-base-100 w-90 shadow-xl">
+      <figure>
+        <img
+          src={thumbnail}
+          alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title?.substring(0, 20)}...</h2>
+        <span className="text-gray-500 mt-2">${price}</span>
+        <div className="card-actions justify-end">
+        <button
+            className={`btn ${
+              isAddedToCart
+                ? "p-2 rounded-full bg-red-600 text-white   hover:bg-red-500 focus:outline-none focus:bg-red-500 "
+                : "p-2 rounded-full bg-blue-600 text-white   hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
+            }`}
+            onClick={(e) => {
+              e.preventDefault();
+              if (!isAddedToCart) {
+                dispatch(addToCart(resData));
+              } else {
+                dispatch(removeFromCart({ id }));
+              }
+            }}
+          >
+            {isAddedToCart ? (
+              <>
+                <XCircleIcon className="h-5 w-5"   />
+                
+              </>
+            ) : (
+              <>
+                <ShoppingCartIcon className="h-5 w-5"  />
+                
+              </>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+    </Link> */}
+      <Link to={"/product/" + id}>
+        <div className="card glass w-90">
+          <figure>
+            <img src={thumbnail} alt={title} loading="lazy" />
+          </figure>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              if (!isFavourite) {
+                dispatch(addToFavourite(resData));
+              } else {
+                dispatch(removeFromFavourite({ id }));
+              }
+            }}
+            className="absolute top-2 right-2 focus:outline-none mt-2"
+          >
+            {isFavourite ? (
+              <HeartIconSolid className="w-6 h-6 text-red-500" />
+            ) : (
+              <HeartIconOutline className="w-6 h-6 text-gray-400" />
+            )}
+          </button>
+          <div className="card-body">
+            <h2 className="card-title">{title?.substring(0, 20)}...</h2>
+            <p>${price}</p>
+            <div className="card-actions justify-end">
+              <button
+                className={`btn ${
+                  isAddedToCart
+                    ? "btn btn-circle btn-outline text-error hover:bg-error"
+                    : "btn btn-circle btn-outline text-primary hover:bg-primary"
+                }`}
+              >
+                {isAddedToCart ? (
+                  <>
+                    <XCircleIcon className="h-5 w-5" />
+                  </>
+                ) : (
+                  <>
+                    <ShoppingCartIcon className="h-5 w-5" />
+                  </>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </>
   );
 };
 
